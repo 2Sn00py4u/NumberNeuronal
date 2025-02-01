74afee2ef5ca87c594183c81914c9a53fb96096d
StewieOS <Felix.Werner.T@gmx.de>
2025-02-01 09:18:46 +0100

    init

---
+++ ImageStyling.py
@@ -0,0 +1,53 @@
+from PIL import Image
+import numpy as np
+import shutil, os
+
+def scaling(number, scale_now:tuple, scale_new:tuple):
+    range_now = scale_now[1] - scale_now[0]
+    range_new = scale_new[1] - scale_new[0]
+    number_new = ((number/range_now)*range_new) + scale_new[0]
+    return number_new
+
+def resize_image(image_path:str, format:tuple):
+    if os.path.exists(image_path):
+        shutil.copyfile(image_path, os.path.join(os.path.dirname(image_path),"image_original_" + image_path.rsplit("\\")[len(image_path.rsplit("\\"))-1]))
+        with Image.open(image_path) as img:
+            resized_img = img.resize(format)
+            resized_img.save(image_path)      
+    else:
+        raise Exception("Image doesn't exists!")
+    
+def grayscale_Image_to_array(image_path:str, debug:bool = False, size = (48,48), **kwargs):
+    with Image.open(image_path) as img_file:
+        print(img_file.size)
+        if img_file.size != size:
+            resize_image(image_path, size)
+            
+    if kwargs:
+        if kwargs["scale"]:
+            image_scale = kwargs["scale"]
+        else:
+            raise Exception("wrong **kwarg")
+    
+    image = Image.open(image_path)
+    try:
+        grey_image = image.convert("L")
+    except:
+        raise Exception("couldn't convert it into greyscale")
+    
+    if debug:
+        grey_image.show()
+    numpy_array = np.array(grey_image)
+    if debug:
+        print(numpy_array.shape)
+        
+    if image_scale:
+        scaled_numpy_array = []
+        for i in range(len(numpy_array)):
+            scaled_line = []
+            for a in range(len(numpy_array[i])):
+                scaled_line.append(scaling(numpy_array[i][a], (0,255), kwargs["scale"]))
+            scaled_numpy_array.append(scaled_line)
+        return scaled_numpy_array    
+    else:
+        return numpy_array
---
+++ neuronalNumbers.py
@@ -0,0 +1,70 @@
+import ImageStyling as IM
+import os
+
+class number_neuronalNetwork():
+    def __init__(self, Images:list) -> None:
+        self.zeromap = {
+            0: [],
+            1: [],
+            2: [],
+            3: [],
+            4: [],
+            5: [],
+            6: [],
+            7: [],
+            8: [],
+            9: []
+        }
+        self.train_data = {}
+        for i in range(len(Images)):
+            self.train_data[i] = IM.grayscale_Image_to_array(image_path=Images[i], shrink=False, scale=(0,1))
+
+        
+        for number in self.train_data:
+            for i, line in enumerate(self.train_data[number]):
+                for a, pixel in enumerate(line):
+                    if pixel == 0.0:
+                        self.zeromap[number].append((i, a))
+                        
+        #print(self.zeromap)
+                            
+        
+        
+    def train_model(self):
+        pass
+
+    def predict_model(self, Image_path_to_predict):
+        # load zero_map of image to predict
+        zeromap_predict_image = []
+        Image_to_predict = IM.grayscale_Image_to_array(image_path=Image_path_to_predict, shrink=True, scale=(0,1))
+        for i, line in enumerate(Image_to_predict):
+            for a, pixel in enumerate(line):
+                if pixel == 0.0:
+                    zeromap_predict_image.append((i, a))
+        count_zero = len(zeromap_predict_image)
+        print(zeromap_predict_image, count_zero)
+                    
+        #compare to each number
+        output = {}
+        for number in self.zeromap:
+            equal_pixel = 0
+            for pixel in zeromap_predict_image:
+                if pixel in self.zeromap[number]:
+                    equal_pixel +=1
+            output[number] = (equal_pixel/count_zero) *100
+            
+        return output
+            
+            
+            
+
+images=[]
+img_list = os.listdir("NumberNeuronal\\traindata")
+for i in range(len(img_list)):
+    if img_list[i] != "train_numbers.csv":
+        images.append(os.path.join("NumberNeuronal\\traindata", img_list[i]))
+
+numberNeuro = number_neuronalNetwork(images)
+output = numberNeuro.predict_model("NumberNeuronal\\test\\fünf.png")
+for key in output:
+    print(f"{key}: {output[key]}%")
